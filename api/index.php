cadd856c58544bae2257b0f5e3f5cc19ebb09b65
MateusRSdev <mateusRSdev@gmail.com>
2023-09-22 19:44:01 -0300

    instalado composer

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
-<?php
+<?php declare(strict_types = 1)
 /**
  * Here is the serverless function entry
  * for deployment with Vercel.
  */
-require __DIR__.'/../public/index.php';
+phpinfo();
+// require __DIR__.'/../public/index.php';
