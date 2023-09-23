db938c569c7afc443dc575d51f1035eacdab835f
MateusRSdev <mateusRSdev@gmail.com>
2023-09-23 10:42:15 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("/form",[indexController::class,"post"]);
+Route::post("/",[indexController::class,"post"]);
