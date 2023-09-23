12b929c362cfffe2ca6569211a438c6b2e33acb4
MateusRSdev <mateusRSdev@gmail.com>
2023-09-23 07:09:30 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -3,7 +3,6 @@
  * Here is the serverless function entry
  * for deployment with Vercel.
  */
-echo "chegamos ate aqui";
-die();
 
-// require __DIR__.'/../public/index.php';
+
+require __DIR__.'/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "aaaaaa";
+die();
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
