d64b50f3041394cee61fd6014ac6dd09bf73e5be
MateusRSdev <mateusRSdev@gmail.com>
2023-09-23 09:47:37 -0300

    testando database

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
     public function index(Request $request){
-        return "aaaaa";
+        return User::all();
     }
 }
