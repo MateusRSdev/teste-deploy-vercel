f09e9ff7cc98ae9ada642ebd7d142fa55f22d747
MateusRSdev <mateusRSdev@gmail.com>
2023-09-23 07:03:25 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
-<?php declare(strict_types = 1)
+<?php 
 /**
  * Here is the serverless function entry
  * for deployment with Vercel.
  */
-phpinfo();
-// require __DIR__.'/../public/index.php';
+
+require __DIR__.'/../public/index.php';
